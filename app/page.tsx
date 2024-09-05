850ed031d7805f760352bce4577b73c4cc5629f3
seyitkulov <islamsejtkulov@gmail.com>
2024-09-05 16:11:09 +0600

    home page start

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-'use client';
-import { Product } from '../types/common';
 import Header from '@/components/Header';
+<<<<<<< Updated upstream
 import Image from 'next/image';
 import React, { useEffect, useState } from 'react';
 import { productsAPI } from '@/lib/product';
 import { formatCurrency } from '@/utils/formatCurrency';
+=======
+import Footer from '@/components/Footer';
+import Hero from '@/components/Hero/Hero';
+>>>>>>> Stashed changes
 
 const Home: React.FC = () => {
   const [products, setProducts] = useState<Product[]>([]);
@@ -20,6 +23,7 @@ const Home: React.FC = () => {
   return (
     <>
       <Header />
+<<<<<<< Updated upstream
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
           <h2 className="text-3xl font-bold">Products</h2>
@@ -52,6 +56,10 @@ const Home: React.FC = () => {
           </ul>
         )}
       </main>
+=======
+      <Hero />
+      <Footer />
+>>>>>>> Stashed changes
     </>
   );
 };
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,6 +5,7 @@ import logo from '@/public/assets/logo.png';
 
 const Header: React.FC = () => {
   return (
+<<<<<<< Updated upstream
     <header className="w-full py-4 bg-gray-800 text-white">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center cursor-pointer">
@@ -14,6 +15,60 @@ const Header: React.FC = () => {
             </a>
           </Link>
           <span className="ml-2 text-xl font-bold">My Shop</span>
+=======
+    <>
+      <div className="wrap">
+        <div className="header__content">
+          <div className="header__language">
+            <div className="container">
+              <div className="header__content__top">
+                <div className="header__language__text">
+                  Summer Sale For All Swim Suits And Free Express Delivery - OFF
+                  50%!<a>ShopNow</a>
+                </div>
+                <div className="header__language__choice">
+                  English
+                  <GoChevronDown />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="header__head">
+          <div className="container">
+            <div className="header__head__content">
+              <div className="header__head__logo">
+                <h1>Exclusive</h1>
+              </div>
+              <div className="header__head__nav">
+                <a href="#">Home</a>
+
+                <a href="#">Shop</a>
+
+                <a href="#">About</a>
+
+                <a href="#">Contact</a>
+              </div>
+              <div className="header__head__search">
+                <input
+                  className="header__head__search__input"
+                  type="text"
+                  placeholder="What are you looking for?
+"
+                />
+                <span>
+                  <CiHeart />
+                </span>
+                <span>
+                  <BiCartAlt />
+                </span>
+                <span>
+                  <IoPersonCircleOutline />
+                </span>
+              </div>
+            </div>
+          </div>
+>>>>>>> Stashed changes
         </div>
         <nav>
           <Link href="/login" legacyBehavior>
@@ -21,7 +76,12 @@ const Header: React.FC = () => {
           </Link>
         </nav>
       </div>
+<<<<<<< Updated upstream
     </header>
+=======
+      <hr className="headerHr" />
+    </>
+>>>>>>> Stashed changes
   );
 };
 
