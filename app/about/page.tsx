a6fd4c1ef07e4e582d2ad6e352c65e1d9bd2022e
seyitkulov <islamsejtkulov@gmail.com>
2024-09-14 10:58:00 +0600

    aboutUI finish

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,12 +2,18 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import '../../app/globals.css';
 import AboutStory from '@/components/AboutStory';
+import AboutActive from '@/components/AboutActive';
+import AboutEmployees from '@/components/AboutEmployees';
+import AboutServies from '@/components/AboutServies';
 
 const About: React.FC = () => {
   return (
     <>
       <Header />
       <AboutStory />
+      <AboutActive />
+      <AboutEmployees />
+      <AboutServies />
       <Footer />
     </>
   );
---
+++ components/AboutServies/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import servies from '../../public/assets/Services.png';
+import serviesHeadPhone from '../../public/assets/Services-headPhone.png';
+import serviesCheck from '../../public/assets/Services-check.png';
+import Image from 'next/image';
+import '../AboutServies/style.css';
+function AboutServies() {
+  return (
+    <>
+      <div id="servies">
+        <div className="container">
+          <div className="servies">
+            <div className="first-servies">
+              <Image src={servies} alt="footerimg" />
+              <h3>FREE AND FAST DELIVERY</h3>
+              <p>Free delivery for all orders over $140</p>
+            </div>
+            <div className="second-servies">
+              <Image src={serviesHeadPhone} alt="footerimg" />
+              <h3>24/7 CUSTOMER SERVICE</h3>
+              <p>Friendly 24/7 customer support</p>
+            </div>
+            <div className="third-servies">
+              <Image src={serviesCheck} alt="footerimg" />
+              <h3>MONEY BACK GUARANTEE</h3>
+              <p>We reurn money within 30 days</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default AboutServies;
